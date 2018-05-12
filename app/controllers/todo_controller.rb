8340bf91227fff1ee3217d09321e3e4e6c669a21
Michelle Nnebe <michellennebe@gmail.com>
2018-05-12 19:16:52 +0000

    the rest of the conditional logic

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -9,7 +9,22 @@ class TodoController < ApplicationController
         elsif todo_id == '2'
             @todo_description = "Buy workshop supplies"
             @todo_pomodoro_estimate = 3
-        end 
+        elsif todo_id == '3' # Remember, the `==` conditional operator is used to check if two things are equal to each other.
+            @todo_description = "Meet with the volunteer trainers"
+            @todo_pomodoro_estimate = 4
+        elsif todo_id == '4'
+        	@todo_description = "Order food for Saturday and Sunday"
+        	@todo_pomodoro_estimate = 2
+        elsif todo_id == '5'
+        	@todo_description = "Check pre-work assignments"
+        	@todo_pomodoro_estimate = 2
+        elsif todo_id == '6'
+        	@todo_description = "Send workshop location to all the students"
+        	@todo_pomodoro_estimate = 2
+        elsif todo_id == '7'
+        	@todo_description = "Have a great workshop"
+        	@todo_pomodoro_estimate = 2
+        end
         
     end
 end    
